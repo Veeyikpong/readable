e5a28970bfbe6ddbf04d618e43bae6f91e870b03
Veeyikpong <veeyikpong@gmail.com>
2018-02-01 17:45:05 +0800

    1. Dummy reducer and action 2. Basic API Integration

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,22 +3,34 @@ import logo from '../assets/images/logo.ico';
 import sortIcon from '../assets/images/sort.png'
 import '../assets/App.css';
 import Post from './Post.js'
+import * as ReadableAPI from '../utils/api.js'
 
 class App extends Component {
+
+  componentDidMount()
+  { 
+      ReadableAPI.getAllPosts().then((posts)=>{
+        this.setState(posts)
+      })
+  }
+
+   state = {
+    posts : []
+  }
+
   render() {
     return (
       <div className="App">
 
-        <div id="header" class="container">
+        <div id="header" className="container">
 
           <div id="logo">
             <img src={logo} className="App-logo" alt="logo" />
             <h1><a href="#">Readable</a></h1>
           </div>
 
-            <button className='button'>Create</button>
-            <button className="button-sorticon"></button>
-           
+          <button className='button'>Create</button>
+          <button className="button-sorticon"></button>
           
           <Post/>
         </div>
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -6,17 +6,17 @@ import '../assets/App.css';
 class Post extends Component {
   render() {
     return (
-        <div id="post" class="post">
-	        <h3 class="title">Post Title</h3>
+        <div id="post" className="post">
+	        <h3 className="title">Post Title</h3>
 	        <span>By </span>
-	        <span class="author">Author</span>
-	        <p class="description">Post Description</p>
+	        <span className="author">Author</span>
+	        <p className="description">Post Description</p>
 
 	        <img className="vote" src={upvoteIcon} alt="logo" />
 			<img className="vote" src={downvoteIcon} alt="logo" />
-	        <p class="votescore">1 Votes</p>
-	        <p class="comments">1 comments</p>
-	        <p class="timestamp">{new Date().toLocaleString()}</p>
+	        <p className="votescore">1 Votes</p>
+	        <p className="comments">1 comments</p>
+	        <p className="timestamp">{new Date().toLocaleString()}</p>
       	</div>
     );
   }
