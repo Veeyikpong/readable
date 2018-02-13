3ffe88dc8c6b905f799d9dabccad8b857f3281eb
Pong <veeyikpong@gmail.com>
2018-02-13 16:56:20 +0800

    feat: Add copyright footer
    Add copyright footer and package.json file for easy installation

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer className="footer">
+        <small>&copy; Developed by Pong Vee Yik @ Udacity React Programme</small>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
